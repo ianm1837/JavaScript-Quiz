b9ef3cdf24de6ba8a6a166818452031f2130534e
ianm1837 <ian@iwm.dev>
2022-11-12 00:52:50 -0800

    added questions to script.js

---
+++ Scripts/script.js
@@ -0,0 +1,52 @@
+//create array of objects with questions
+var questions = [{
+		number: 1,
+		content: "commonly used data types in JS do not include",
+		answer1: "strings",
+		answer2: "booleans",
+		answer3: "numbers",
+		answer4: "alerts",
+		correct: "answer4"		
+	},
+	{
+		number: 2,
+		content: "the condition in an if/else statement is enclosed with _____________.",
+		answer1: "quotes",
+		answer2: "curly brackets",
+		answer3: "parenthesis",
+		answer4: "square brackets",
+		correct: "answer3"
+	},
+	{
+		number: 3,
+		content: "Arrays in JavaScript can be used to store _____________.",
+		answer1: "numbers and strings",
+		answer2: "other arrays",
+		answer3: "booleans",
+		answer4: "all of the above",
+		correct: "answer4"
+	},
+	{
+		number: 4,
+		content: "String values must be enclosed within ___________ when being assigned to variables.",
+		answer1: "commas",
+		answer2: "curly brackets",
+		answer3: "quotes",
+		answer4: "parenthesis",
+		correct: "answer3"
+	},
+	{
+		number: 5,
+		content: "A very useful tool used during development and debugging for printing content to the debugger is:",
+		answer1: "JavaScript",
+		answer2: "terminal/bash",
+		answer3: "for loops",
+		answer4: "console.log",
+		correct: "answer4"
+	}
+]
+
+//create timer using setInterval()
+
+
+//create function for reading and writing score
